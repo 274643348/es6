b00fe366dbffb7fccd8bfee9e5cfce8d10dcabfa
XiaoYan <liujingyan@congmingpai.com>
2018-09-14 18:23:29 +0800

    模拟韩俊在ccc中调用aaa的reject时,aaa中的FuncB还会继续执行

--- await/await被reject.js
+++ await/await被reject.js
@@ -1,32 +1,57 @@
 async function funcA(){
-    console.log("11111 ");
+    console.log("11111 A");
     return new Promise((resolve,reject)=>{
-    console.log("11111 promise");
-     // reject("11111");
-     resolve("11111");
+    console.log("11111 A promise");
+     //reject("11111");
+     setTimeout(resolve,"11111 A",1000);
    });
  
  };
  
  async function funcB ()
  {
-   console.log("22222 ");
+   console.log("22222 B");
    return new Promise((resolve,reject)=>{
-    console.log("22222 promise");
+    console.log("22222 B promise");
      // reject("22222");
-     resolve("22222");
+     resolve("22222 B");
    });
  }
  
- async function aaaa()
+let rejectL;
+
+async function aaaa() {
+
+  return new Promise(async(resolve, reject) => {
+    rejectL = reject;
+    console.log("11111 ");
+    await funcA();
+    console.log("22222 ");
+    await funcB();
+    console.log("33333 ");
+
+    resolve()
+  });
+}
+ 
+ async function bbbb()
  {
-   console.log("11111 ");
-   await funcA();
-   console.log("22222 ");
-   await funcB();
-   console.log("33333 ");
+    let aaaaP = await aaaa().then((value)=>{
+      console.log("aaa ---  then  ---" + value);
+
+    },(err)=>{
+      console.log("aaa ---  then  ---" + err);
+    });
  }
  
+ async function cccc()
+ {
+  console.log("ccccc");
+    rejectL();
+ }
+ 
+
  console.log("00000 ");
- aaaa();
+ bbbb();
+ cccc();
  console.log("44444 ");
